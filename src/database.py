3b836402676c912833dfe5386ef48c2065f6bb6f
bodyauza <bogdanazino777@gmail.com>
2025-07-02 17:40:10 +0300

    Updated authentication endpoints.

--- src/database.py
+++ src/database.py
@@ -3,14 +3,15 @@ from sqlalchemy import MetaData, NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://root:123@localhost:5432/clients"
+from src.config import settings
+
 
 class Base(DeclarativeBase): pass
 
 metadata = MetaData()
 
 # Создаем асинхронный движок для подключения к базе данных с использованием NullPool (без пула соединений)
-engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+engine = create_async_engine(settings.ASYNC_DATABASE_URL, poolclass=NullPool)
 
 # Создаем фабрику асинхронных сессий с использованием ранее созданного движка
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
